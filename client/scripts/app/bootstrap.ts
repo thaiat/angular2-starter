88317883269ecbda06674962efafb1c7071b7e72
Avi Haiat <thaiat@ipelia.fr>
2015-12-30 11:33:40 +0200

    feat(app): Add router

--- client/scripts/app/bootstrap.ts
+++ client/scripts/app/bootstrap.ts
@@ -1,6 +1,12 @@
+/* beautify ignore:start */
+import {provide} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {HTTP_PROVIDERS} from 'angular2/http';
 import {AppComponent} from './app.component';
+/* beautify ignore:end */
+
 //import {enableProdMode} from 'angular2/core';
 //enableProdMode();
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS, provide(LocationStrategy, { useClass: HashLocationStrategy })]);
